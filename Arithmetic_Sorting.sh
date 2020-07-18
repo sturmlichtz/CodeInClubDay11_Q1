53157cfd1708008ecf294d555f65417f484ac54b
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-18 22:22:13 +0530

    values are stored in array from dictionary

--- Arithmetic_Sorting.sh
+++ Arithmetic_Sorting.sh
@@ -9,3 +9,8 @@ dictForValue[2]=$(($a*$b+$c))
 dictForValue[3]=$(($c+$a/$b))
 dictForValue[4]=$(($a%$b+$c))
 echo "Values in Dictionary" ${dictForValue[@]}
+for((i=1;i<=4;i++))
+do
+	arrayForRandom[$i]=${dictForValue[$i]}
+done
+echo "Values in Array" ${arrayForRandom[@]}

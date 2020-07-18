80ee5751495083f211e754542c8fb6d2f11edfe4
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-18 21:46:29 +0530

    value of first Computation stored

--- Arithmetic_Sorting.sh
+++ Arithmetic_Sorting.sh
@@ -3,3 +3,5 @@ echo "welcome to Arithmetic Sorting "
 read -p "Enter the value for varible A :- " a
 read -p "Enter the value for varible B :- " b
 read -p "Enter the value for varible C :- " c
+Value1=$(($a+$b*$c))
+echo $Value1

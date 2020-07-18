97e5de055773f54ac0218c2d8e776c60d9ae5eba
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-18 20:48:26 +0530

    taken a three input

--- Arithmetic_Sorting.sh
+++ Arithmetic_Sorting.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "welcome to Arithmetic Sorting "
+read -p "Enter the value for varible A :- " a
+read -p "Enter the value for varible B :- " b
+read -p "Enter the value for varible C :- " c

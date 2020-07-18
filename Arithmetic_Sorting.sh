35e480d09fbe841c535154818ce55cd027ec431b
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-18 22:15:44 +0530

    results are stored in a Dictionary

--- Arithmetic_Sorting.sh
+++ Arithmetic_Sorting.sh
@@ -3,11 +3,9 @@ echo "welcome to Arithmetic Sorting "
 read -p "Enter the value for varible A :- " a
 read -p "Enter the value for varible B :- " b
 read -p "Enter the value for varible C :- " c
-Value1=$(($a+$b*$c))
-echo $Value1
-Value2=$(($a*$b+$c))
-echo $Value2
-Value3=$(($c+$a/$b))
-echo $Value3
-Value4=$(($a%$b+$c))
-echo $Value4
+declare -A dictForValue
+dictForValue[1]=$(($a+$b*$c))
+dictForValue[2]=$(($a*$b+$c))
+dictForValue[3]=$(($c+$a/$b))
+dictForValue[4]=$(($a%$b+$c))
+echo "Values in Dictionary" ${dictForValue[@]}

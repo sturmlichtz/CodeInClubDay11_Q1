4b2314ef7ef6417f1e49621e8873d3367889a2a1
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-18 22:46:14 +0530

    conflict solved

--- Arithmetic_Sorting.sh
+++ Arithmetic_Sorting.sh
@@ -27,8 +27,7 @@ do
 		fi
 	done
 done
-<<<<<<< HEAD
-echo "Results are sorted in Descending order :- " ${arrayForRandom[@]}
+echo "Result is sorted in Descending order :- " ${arrayForRandom[@]}
 for ((i=0;i<=$arrayLength;i++))
 do
 	for ((j=$(($i+1)); j<=$arrayLength;j++))
@@ -42,6 +41,3 @@ do
 	done
 done
 echo "Results is sorted in Ascending order :- " ${arrayForRandom[@]}
-=======
-echo "Result is sorted in Descending order :- " ${arrayForRandom[@]}
->>>>>>> SortInDescendingOrder
